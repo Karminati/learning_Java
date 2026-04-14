785f0c66a6c83c0108274fcfcdef36bde02c3719
Karminati <gabrielm.carminati@gmail.com>
2026-04-14 10:47:47 -0300

    14/04/2026, 3rd  COTD(commit of the day), fisrt server definitions, would do more things but i have to go to college now :(, did lots of progress i think though

--- object_Orientation/aplicativo_Banco/appBanco.md
+++ object_Orientation/aplicativo_Banco/appBanco.md
@@ -2,7 +2,7 @@
 ## Documentation:
 - This is a object orientation project made thinking of how a banking system might work
 - I dont intend to make it too complicated but let's see where i stop ;)
-- - I also dont intend to use rewriting and polymorphism
+ - I also dont intend to use rewriting and polymorphism
 ## From this part on i will put some of the system decisions i made:
 - I will make a "server" class to handle the system workflow
-- - It will have server id and password as well as methods to listen to users and handle database stuff (local database)
+ - It will have server id and password as well as methods to listen to users and handle database stuff (local database)
---
+++ object_Orientation/aplicativo_Banco/models/Server.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+class Server
+{
+    long id;
+    String password;
+    int aplicationRequests;
+    int aplicationStatus;
+
+    Server(long id, String password, int aplicationStatus)
+    {
+        this.id = id;
+        this.password = password;
+        this.aplicationStatus = aplicationStatus;
+        if(aplicationStatus == 1)
+        {
+            this.aplicationRequests++;
+            runServer(aplicationStatus);
+        }
+    }
+
+    void runServer(int status)
+    {
+        Scanner scan = new Scanner(System.in);
+        if(status == 0)
+        {
+            System.out.println("The request to run the server failed, check the value of the request");
+        }
+        else if(status == 1)
+        {
+            boolean condicao = true;
+            while(condicao)
+            {
+                System.out.println("Type in desired operation:");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("0 - Terminate server (server password necessary)");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("1 - Create Account");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("2 - Log in account");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("3 - See application status (server password necessary)");
+                System.out.println("--------------------------------------------------------");
+                int op = scan.nextInt();
+                this.aplicationRequests++;
+                if(op == 0)
+                {
+                    System.out.println("Please type in the server password: ");
+                    String inserted = scan.next();
+                    if(this.password.equals(inserted))
+                    {
+                        condicao = false;
+                        System.out.println("Terminating server operation");
+                    }
+                }
+                else if(op == 1)
+                {
+                    System.out.println("Do you already have a user? (Y/n)");
+                    char confirm = scan.next().charAt(0);
+                    if(confirm == 'Y')
+                    {
+
+                    }
+                    else if(confirm == 'n')
+                    {
+
+                    }
+                }
+                else if(op == 2)
+                {
+
+                }
+                else if(op == 3)
+                {
+                    System.out.println("Please type in the server password: ");
+                    String inserted = scan.next();
+                    if(this.password.equals(inserted))
+                    {
+                        System.out.printf("The server received %d requests\n", this.aplicationRequests);
+                        System.out.printf("The current server status is %d\n", this.aplicationStatus);
+                        System.out.printf("(%d) is the server identification", this.id);
+                    }
+                }
+            }
+        }
+    }
+}
